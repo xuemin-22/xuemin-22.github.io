78fe654537bee450089d479e000a9181404bb8e9
1269095352zhao-lgtm <1269095352zhao@gmail.com>
2026-01-16 15:18:48 +0800

    Site updated: 2026-01-16 15:18:46

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["1970/01/01/hello-world/","2026/01/14/这是一个测试/"];function toRandomPost(){
+var posts=["2026/01/14/这是一个测试/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
