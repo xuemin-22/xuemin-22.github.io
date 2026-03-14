883443618f38b6cd0242a3a2038bdd41b31e5bd4
1269095352zhao-lgtm <1269095352zhao@gmail.com>
2026-03-14 21:41:25 +0800

    Site updated: 2026-03-14 21:41:24

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["posts/12874/","posts/1b1497bb/"];function toRandomPost(){
+var posts=["posts/12874/","posts/1b1497bb/","posts/eff378d3/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
